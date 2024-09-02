3fbec74280e1c0506afcbbfe4c1b29959528b0aa
Eeshan4 <eeshan.vaghjiani@strathmore.edu>
2024-09-02 05:43:43 +0300

    week 2.2

--- fnc.php
+++ fnc.php
@@ -1,7 +1,6 @@
 <?php
 class fnc{
     var $name;
-
     public $fname;
     public $yob;
     public $age;
--- index.php
+++ index.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test 1
-    
-    </title>
-</head>
-<body>
 <?php
-require_once "load.php";
-// print "Hi ! my Name Is ".$obj -> computer_user("Eeshan")." And I Am ".$obj ->user_age(2004) . " Years Old. ";
-print $obj->user_age("Eeshan", 2004);
-print "<br>";
-print $obj->hash_pass('123');
-?>
-    
-</body>
-</html>
+    require_once "load.php";
+    $ObjLayouts->heading();
+    $ObjMenus->main_menu();
+    print "<br><br><br><br><br><br><br><br><br>";
+    $ObjLayouts->footer();
