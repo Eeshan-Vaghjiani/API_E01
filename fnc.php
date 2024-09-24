dde6ce846fd05d561822983e2b56c017fc8f99b4
Eeshan-Vaghjiani <101983223+Eeshan-Vaghjiani@users.noreply.github.com>
2024-09-24 19:20:11 +0300

    test

--- fnc.php
+++ fnc.php
@@ -9,14 +9,12 @@ class fnc{
     public function computer_user($fname){
         return $fname;
     }
-
     // public function hash_pass($pass){
     //     return md5($pass);
     // }
     public function hash_pass($pass){
         return password_hash($pass,PASSWORD_DEFAULT); 
     }
-
     public function user_age ($fname,$yob){
         $user = $this->computer_user($fname);
         $age = date('Y') - $yob;
