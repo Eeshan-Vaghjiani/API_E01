35311a305029ec33a615a1b4d2690e0cb7b70d84
Eeshan-vaghjiani <evaghjiani@gmail.com>
2024-12-09 20:50:00 +0300

    laravel

--- laravel/database/migrations/2024_12_09_174242_add_two_factor_auth_code_to_users_table.php
+++ laravel/database/migrations/2024_12_09_174242_add_two_factor_auth_code_to_users_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('users2', function (Blueprint $table) {
+            $table->string('two_fa_code')->nullable();
         });
     }
 
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::table('users2', function (Blueprint $table) {
+            $table->dropColumn('two_fa_code');
         });
     }
 };
